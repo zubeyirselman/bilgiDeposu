b62b4f590d1ce9026db227aa91bfa11f36e84463
zsk <zsk199207@gmail.com>
2023-03-28 02:59:59 +0300

    yeni proje 2. class

---
+++ src/week_05/C02_githubOrnek2.java
@@ -0,0 +1,9 @@
+package week_05;
+
+public class C02_githubOrnek2 {
+    public static void main(String[] args) {
+        System.out.println("github' a eklenen 3. dosya");
+
+        System.out.println("Bunu kendi basina basarmak cok guzel bir duygu");
+    }
+}
