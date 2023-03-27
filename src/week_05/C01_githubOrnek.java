ca3d8d6d78a80bc98a6bddb2ffc31d23cc12e037
zsk <zsk199207@gmail.com>
2023-03-28 02:57:24 +0300

    yeni proje2

--- src/week_05/C01_githubOrnek.java
+++ src/week_05/C01_githubOrnek.java
@@ -3,5 +3,9 @@ package week_05;
 public class C01_githubOrnek {
     public static void main(String[] args) {
         System.out.println("github' a eklenen dosya");
+
+        System.out.println("github' a eklenen 2. dosya");
+
+
     }
 }
