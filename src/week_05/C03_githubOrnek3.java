a29c41b7ffed933f5b55224dd09f37ca42616693
zsk <zsk199207@gmail.com>
2023-03-28 03:01:58 +0300

    yeni proje 3.class

---
+++ src/week_05/C03_githubOrnek3.java
@@ -0,0 +1,10 @@
+package week_05;
+
+public class C03_githubOrnek3 {
+    public static void main(String[] args) {
+
+        System.out.println("Java Heyecandir");
+
+        System.out.println("En kisa surede offer kapmali");
+    }
+}
